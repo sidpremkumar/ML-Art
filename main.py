f90d5159cebd432e4a389deafc980124ce7c064a
sidpremkumar <sid.premkumar@gmail.com>
2018-11-05 22:32:37 -0500

    working nov 5

--- main.py
+++ main.py
@@ -314,7 +314,7 @@ def driver(content_path,style_path,num_iterations=1000,content_weight=1e3,style_
             #Show the image
             final_image.show()
             #Save the image
-            final_image.save('outputs/' + str(i))
+            final_image.save('outputs/' + str(style_path) + '-'(i) + '.bmp')
             #print('Iteration: {}'.format(i))
             #print('Total loss: {:.4e}, '
             #      'style loss: {:.4e}, '
